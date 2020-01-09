35e4a2568cb9f16c6aeda318b0bfe6c22557d53d
lilianyangc <mlilianyang@gmail.com>
2020-01-08 21:46:35 -0500

    Admin dashboard design added

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { LayoutComponent } from './layout/layout.component';
@@ -7,17 +7,23 @@ import { adminRoutes } from './admin.routes';
 import { RoleGuard } from '../_guards/role-guard.service';
 import { AuthGuard } from '../_guards/auth-guard.service';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from '../material.module';
+
 
 
 @NgModule({
   declarations: [HomeComponent, LayoutComponent],
   imports: [
+    BrowserAnimationsModule,
+    AngularMaterialModule,
     CommonModule,
     RouterModule.forChild(adminRoutes)
   ],
   providers:[
     RoleGuard,
     AuthGuard
-  ]
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AdminModule { }
