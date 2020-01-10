d4fb6915521682592cadd492f1f50ac648772199
lilianyangc <mlilianyang@gmail.com>
2020-01-09 22:18:59 -0500

    Changed index title

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,9 +1,79 @@
 import { Injectable } from '@angular/core';
+import { User } from './user';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { IGame } from '../game';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
 
-  constructor() { }
+export class ApiService {
+
+  endpoint: string = 'http://localhost:4000/user';
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+  private games_url: string ="/assets/data/games.json";
+
+  constructor(private http: HttpClient) { }
+
+  // Add user
+  AddUser(data: User): Observable<any> {
+    let API_URL = `${this.endpoint}/add-user`;
+    return this.http.post(API_URL, data)
+      .pipe(
+        catchError(this.errorMgmt)
+      )
+  }
+
+  // Get all users
+  GetUsers() {
+    return this.http.get(`${this.endpoint}`);
+  }
+
+  // Get user
+  GetUser(id): Observable<any> {
+    let API_URL = `${this.endpoint}/read-user/${id}`;
+    return this.http.get(API_URL, { headers: this.headers }).pipe(
+      map((res: Response) => {
+        return res || {}
+      }),
+      catchError(this.errorMgmt)
+    )
+  }
+
+  // Update user
+  UpdateUser(id, data: User): Observable<any> {
+    let API_URL = `${this.endpoint}/update/${id}`;
+    return this.http.put(API_URL, data, { headers: this.headers }).pipe(
+      catchError(this.errorMgmt)
+    )
+  }
+
+  // Delete user
+  DeleteUser(id): Observable<any> {
+    var API_URL = `${this.endpoint}/delete-user/${id}`;
+    return this.http.delete(API_URL).pipe(
+      catchError(this.errorMgmt)
+    )
+  }
+
+  // Error handling
+  errorMgmt(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+
+  getGames(): Observable<IGame[]>{
+    return this.http.get<IGame[]>(this.games_url);
+  }
 }
