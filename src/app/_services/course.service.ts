688379e994346fb7275ef7177a6bfafecda6d673
lilianyangc <mlilianyang@gmail.com>
2020-01-09 22:16:12 -0500

    Added user service and course service in fe

---
+++ src/app/_services/course.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@angular/core';
+import { Course } from './course';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { IGame } from '../game';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ApiService {
+
+  endpoint: string = 'http://localhost:4000/course';
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+  private games_url: string ="/assets/data/games.json";
+
+  constructor(private http: HttpClient) { }
+
+  // Add course
+  AddCourse(data: Course): Observable<any> {
+    let API_URL = `${this.endpoint}/add-course`;
+    return this.http.post(API_URL, data)
+      .pipe(
+        catchError(this.errorMgmt)
+      )
+  }
+
+  // Get all Courses
+  GetCourses() {
+    return this.http.get(`${this.endpoint}`);
+  }
+
+  // Get course
+  GetCourse(id): Observable<any> {
+    let API_URL = `${this.endpoint}/read-course/${id}`;
+    return this.http.get(API_URL, { headers: this.headers }).pipe(
+      map((res: Response) => {
+        return res || {}
+      }),
+      catchError(this.errorMgmt)
+    )
+  }
+
+  // Update course
+  UpdateCourse(id, data: Course): Observable<any> {
+    let API_URL = `${this.endpoint}/update/${id}`;
+    return this.http.put(API_URL, data, { headers: this.headers }).pipe(
+      catchError(this.errorMgmt)
+    )
+  }
+
+  // Delete course
+  DeleteCourse(id): Observable<any> {
+    var API_URL = `${this.endpoint}/delete-course/${id}`;
+    return this.http.delete(API_URL).pipe(
+      catchError(this.errorMgmt)
+    )
+  }
+
+  // Error handling
+  errorMgmt(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+
+  getGames(): Observable<IGame[]>{
+    return this.http.get<IGame[]>(this.games_url);
+  }
+}
---
+++ src/app/_services/course.ts
@@ -0,0 +1,8 @@
+export class Course {
+    _id: String;
+    name: String;
+    details: String;
+    max_students: String;
+    members: [];
+    instructors: [];
+ }
