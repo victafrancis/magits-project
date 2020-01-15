b70422935caa160bee1080f2c63cac645bb6f503
lilianyangc <mlilianyang@gmail.com>
2020-01-15 18:16:24 -0500

    Made CreateCourse functionality

--- src/app/admin-module/courses/create-course/create-course.component.ts
+++ src/app/admin-module/courses/create-course/create-course.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { CourseService } from '../../../_services/course/course.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-course',
@@ -12,27 +14,41 @@ import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 export class CreateCourseComponent implements OnInit {
 
   courseForm: FormGroup;
-  Days: any = ['1', '2', '3'];
+  numDays: any = [1,2,3,4,5,6,7];
+  Days: any = ['Monday', 'Tuesday', 'Wednesday', 'Thursday','Friday','Saturday','Sunday'];
   day_selected: null;
+  scheduleForm: FormGroup;
   
   constructor(    
-    public fb: FormBuilder
+    public fb: FormBuilder,
+    private courseApi: CourseService,
+    private ngZone: NgZone,
+    private router: Router
   ) { }
 
   ngOnInit() {
-    this.submitCourseForm();
+    this.courseForm = this.fb.group({
+      name: ['', [Validators.required]],
+      details: ['', [Validators.required]]
+      // numDays: ['', Validators.required]
+    })
+
+    // this.scheduleForm= this.fb.group({
+
+    // })
   }
 
-  submitCourseForm(){
-    this.courseForm = this.fb.group({
-      course_name: ['', [Validators.required]],
-      course_detail: ['', [Validators.required]],
-      number_of_days: ['', Validators.required]
-    })
-  }
   /* Get errors */
   public handleError = (controlName: string, errorName: string) => {
     return this.courseForm.controls[controlName].hasError(errorName);
+  }
+
+  submitCourseForm(){
+    if(window.confirm('Are you sure you want to add this course?')){
+      this.courseApi.AddCourse(this.courseForm.value).subscribe(res => {
+        this.ngZone.run(() => this.router.navigateByUrl('\courses'))
+      });
+    }
   }
 }
 
