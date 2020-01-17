c7c7419d3dedb44095e76690d2d29c1c41f0b265
lilianyangc <mlilianyang@gmail.com>
2020-01-17 17:46:44 -0500

    Added schedule form

--- src/app/admin-module/courses/create-course/create-course.component.ts
+++ src/app/admin-module/courses/create-course/create-course.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, NgZone } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { CourseService } from '../../../_services/course/course.service';
 import { Router } from '@angular/router';
+import { Schedule } from './schedule';
+import { User } from 'src/app/_services/user/user';
+import { UserService } from 'src/app/_services/user/user.service';
 
 @Component({
   selector: 'app-create-course',
@@ -15,18 +18,24 @@ export class CreateCourseComponent implements OnInit {
 
   courseForm: FormGroup;
   numDays: any = [1,2,3,4,5,6,7];
+  time: any = [1,2,3,4,5,6,7,8,9,10]
   Days: any = ['Monday', 'Tuesday', 'Wednesday', 'Thursday','Friday','Saturday','Sunday'];
   day_selected: null;
   
+  // used for schedule
   totalDays: number=0;
-  day: null;
   daysWithDate: Array<Object>;
   arrayLen: number;
+  schedule: Array<Schedule>=[]; 
 
+  // used for instructor
+  instructors: any=[];
+  instructor: null;
 
   constructor(    
     public fb: FormBuilder,
     private courseApi: CourseService,
+    private instructorApi: UserService,
     private ngZone: NgZone,
     private router: Router
   ) { }
@@ -40,9 +49,10 @@ export class CreateCourseComponent implements OnInit {
       // numDays: ['', Validators.required]
     })
 
-    // this.scheduleForm= this.fb.group({
+    this.instructorApi.GetInstructors().subscribe( data =>{
+      this.instructors = data;
+    })
 
-    // })
   }
 
   /* Get errors */
@@ -50,6 +60,7 @@ export class CreateCourseComponent implements OnInit {
     return this.courseForm.controls[controlName].hasError(errorName);
   }
 
+  // Creates a course in the database
   submitCourseForm(){
     if(window.confirm('Are you sure you want to add this course?')){
       this.courseApi.AddCourse(this.courseForm.value).subscribe(res => {
@@ -58,12 +69,16 @@ export class CreateCourseComponent implements OnInit {
     }
   }
 
+  // test
   lastAction: string;
+
+  // Checkbox data for the membership type
   data = [
     { label: 'Subscription', checked: false },
     { label: 'Session-based', checked: false },
     ];
 
+  // Detects the changes for the membership type 
   onChange(event, index, item) {
       item.checked = !item.checked;
       this.lastAction = 'index: ' + index + ', label: ' + item.label + ', checked: ' + item.checked;
@@ -71,18 +86,29 @@ export class CreateCourseComponent implements OnInit {
 
   }
 
-  // pushDays(){
+  // Creates the array for the schedule
+  createDays(){
+    if(this.totalDays > 0){
+      this.schedule = [];
+      for(var i = 1;i <= this.totalDays; i++ ){
+        this.schedule.push(new Schedule('','',''));
+      }
+      console.log(this.schedule);
+      // console.log(`name here: ${this.schedule[0].day} start: ${this.schedule[0].start} end: ${this.schedule[0].end}`)
+    }
+  }
 
-  //   let dateName: any;
+  // Sets the values of the Schedule
+  setDays(){
+    if(this.totalDays > 0){
+      for(var i = 1;i <= this.totalDays; i++ ){
+        this.schedule[i-1] = new Schedule(this.schedule[i-1].day,this.schedule[i-1].start,this.schedule[i-1].end);
 
-  //   if(this.totalDays > 0){
+      }
+      console.log(this.schedule);
+      // console.log(`name here: ${this.schedule[0].day} start: ${this.schedule[0].start} end: ${this.schedule[0].end}`)
 
-  //     for(var i =0;i <= this.totalDays; i++ ){
-  //       dateName = {'label':'i'};
-  //       this.arrayLen =this.daysWithDate.push(dateName);
-  //       console.log(this.arrayLen);
-  //     }
-  //   }
-  // }
+    }
+  }
 }
 
